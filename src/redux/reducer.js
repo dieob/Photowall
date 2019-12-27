c2ba30e05f44e42265eefba0435d828c37b1c392
dbaezsfdc <dbaez@salesforce.com>
2019-12-27 11:36:59 -0300

    added comment section fixed, and comment counter

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,23 @@
-import postes from '../data/posts'
+import _posts from '../data/posts'
 import {combineReducers} from 'redux'
 
 
-function comments(state=[], action){
+function comments(state={}, action){
+    switch (action.type){
+        case 'ADD_COMMENT':
+        if (!state[action.postId]){
+            return {...state, [action.postId]: [action.comment]}
+        } else {
+            return {...state, [action.postId]: [...state[action.postId], action.comment]}
+        }
+        
+        default: return state
+    }
     return state
 }
 
 
-function posts(state = postes, action) {
-    console.log(action.index)
+function posts(state = _posts, action) {
 
     switch(action.type){
         case 'REMOVE_POST': return [...state.slice(0, action.index), ...state.slice(action.index+1)]
